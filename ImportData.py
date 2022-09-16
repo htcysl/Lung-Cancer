b0a8cf44b77d293d94ff632bb8add5b257e8f938
Hatice Uysal <maahhaticeuysal@mail.com>
2022-09-16 18:21:09 +0300

    LinearRegression

--- ImportData.py
+++ ImportData.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 df = pd.read_excel("cancer patient data sets.xlsx")
 df.to_csv ("Test.csv", index = None, header=True)
+print(df.shape)
 print(df)
+print(df.describe())
+print(df.values)
