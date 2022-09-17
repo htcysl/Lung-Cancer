5a773270fe93307f8e25f4137711cf958e797a4b
Hatice Uysal <maahhaticeuysal@mail.com>
2022-09-17 15:02:23 +0300

    Create DecisionTree.py

---
+++ DecisionTree.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+
